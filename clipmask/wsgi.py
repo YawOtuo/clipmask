3dfc722701bcd1fdf0edb5dfd695b0af0f565674
YawOtuo <yotuo2003@gmail.com>
2022-02-06 18:46:25 +0000

    pushing to heroku

--- clipmask/wsgi.py
+++ clipmask/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from clipmask.settings import BASE_DIR, STATIC_ROOT, STATIC_URL
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clipmask.settings')
+
 
 application = get_wsgi_application()
